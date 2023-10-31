95f7b0289460f05ca1632a0237765ceb9b39ec84
Tarik SADKHI <tarik.sadkhi@gmail.com>
2023-10-31 11:15:35 +0100

    MVP V2

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractclassmethod
+import random
 
 
 
@@ -6,12 +7,9 @@ class Character:
     def __init__(self, nom : str, pv: int) -> None:
         self.nom = nom
         self.pv = pv
-       
-    
-    def attack():
-        pass
     
     
-    
-
+    def attack(self, opposant):
+        random_attack = random.randint(10, 20)
+        opposant.pv -= random_attack
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from character import Character
 from enemy import Enemy
 from hero import Hero
 
@@ -7,8 +8,18 @@ def main():
     hero = Hero(hero_name)
     monster_1 = Enemy()
     
+    print("######")
     print(hero)
     print(monster_1)
+    print("######")
+    
+    hero.attack(monster_1)
+    
+    print(monster_1)
+    
+    monster_1.attack(hero)
+    
+    print(hero)
     
     
     
